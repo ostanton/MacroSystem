f02186d01cf3928ecd1479412cf7d8e8644cd013
ostanton <114495956+ostanton@users.noreply.github.com>
2025-02-21 19:10:15 +0000

    Added precompile stuff in Build.cs
    Seems like it's helpful, idk

--- Source/MacroSystem/MacroSystem.Build.cs
+++ Source/MacroSystem/MacroSystem.Build.cs
@@ -7,6 +7,8 @@ public class MacroSystem : ModuleRules
 	public MacroSystem(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		PrecompileForTargets = PrecompileTargetsType.Any;
+		bUsePrecompiled = true;
 		
 		PublicDependencyModuleNames.AddRange(
 			new string[]
